96c63dadd6bdd24ba19a1ce4ed02fa3e6cc0e445
hanshungithub <hshanshun@163.com>
2019-09-04 08:24:17 +0800

    actor

--- src/main/scala/akkas/t1/SayHelloActorTest.scala
+++ src/main/scala/akkas/t1/SayHelloActorTest.scala
@@ -4,7 +4,11 @@ import akka.actor.{ActorRef, ActorSystem, Props}
 
 object SayHelloActorTest {
 
+  //创建ActorSystem，专门用于管理Actor
   private val actorfactory = ActorSystem("actorfactory")
+  //Props[SayHelloActor]通过反射创建了一个SayHelloActor的实例 "sayHelloActor" 是给Actor起名
+  //sayHelloActorRef: ActorRef 就是Props[SayHelloActor]的ActorRef
+  //actorfactory使用actorfactory.actorOf接管Actor
   private val sayHelloActorRef: ActorRef = actorfactory.actorOf(Props[SayHelloActor],"sayHelloActor")
 
   def main(args: Array[String]): Unit = {
